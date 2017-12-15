1dfaaf2f6e56558e487e66157a0c2fa26e79e0bd
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-15 14:26:43 -0500

    Updated from Brython Server: 12/15/2017 2:26:51 PM

--- battleship.py
+++ battleship.py
@@ -9,6 +9,13 @@ EMPTY = 0
 MISS = 1
 
 def buildBoard():
+    for row in range(0,4):
+        for col in range(0,4):
+            print(board[row][col],' ',end = '')
+        print()
+    board = [["a","b","c","d","e"],['f','g','h','i','j'],['k','l','m','n','o']
+    printBoard()
+
     
 def redrawAll():
 
