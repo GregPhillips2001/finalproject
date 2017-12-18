5cc8e4d41c00377a1d0bb273564f4abc725b203d
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-18 14:34:59 -0500

    Updated from Brython Server: 12/18/2017 2:34:59 PM

--- battleship.py
+++ battleship.py
@@ -37,10 +37,11 @@ if __name__ == '__main__':
     
     red = Color(0xff0000,1)
     black = Color(0x000000,1)
+    blue = Color(0x0000ff,1)
     
     blackOutline = LineStyle(1,black)
     
-    circle = CircleAsset(RADIUS,blackOutline)
+    circle = CircleAsset(RADIUS,blackOutline,blue)
     redCircle = CircleAsset(RADIUS,blackOutline,red)
     
     buildBoard()
