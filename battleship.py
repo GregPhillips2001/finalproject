57406291bf9fc3c139d3e4882c8f997888b41d97
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-18 14:47:05 -0500

    Updated from Brython Server: 12/18/2017 2:47:04 PM

--- battleship.py
+++ battleship.py
@@ -20,7 +20,7 @@ def redrawAll():
         item.destroy()
     for row in range(0,5):
         for col in range(0,5):
-            Sprite(circle,(RADIUS*2*row, RADIUS*2*col))
+            Sprite(circle,(RADIUS*2*row+RADIUS, RADIUS*2*col+RADIUS))
             
 
 #def pickComputerShips():
