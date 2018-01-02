1d30b5f1a21a66cb0cf19fe8bde56ba6fafb45a7
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2018-01-02 14:46:02 -0500

    Updated from Brython Server: 1/2/2018 2:46:00 PM

--- battleship.py
+++ battleship.py
@@ -22,6 +22,8 @@ def redrawAll():
             item.destroy()
         userText = TextAsset("Player",fill=black,style="bold 40pt Times") 
         Sprite(userText, (110,375))
+        computerText = TextAsset("Computer",fill=black,style="bold 40pt Times") 
+        Sprite(computerText, (515,375))
         for row in range(0,5):
             for col in range(0,5):
                 Sprite(circle,(RADIUS*2*row+RADIUS, RADIUS*2*col+RADIUS))
@@ -56,8 +58,8 @@ def mouseClick(event):
             if event.x <= (RADIUS*10)+400 and event.x >= (RADIUS*10)+50 and event.y <= RADIUS*10: #only runs if player clicked inside the x coordinates of the game
                 roww = event.x//70
                 coll = event.y//70
-                if data["computerBoard"][roww][coll] != BOAT:
-                    data["computerBoard"][roww][coll] = MISS
+                if data["ComputerBoard"][roww][coll] != BOAT:
+                    data["ComputerBoard"][roww][coll] = MISS
                     Sprite(whiteCircle, (RADIUS+2*coll*RADIUS, RADIUS+2*roww*RADIUS))
         
 
@@ -66,7 +68,7 @@ if __name__ == '__main__':
     data["gameover"] = False
     data["userBoard"] = buildBoard()
     data["CompShips"] = buildBoard()
-    data["ComputerShips"] = buildBoard()
+    data["ComputerBoard"] = buildBoard()
     data["playerShips"] = 0 
 
     red = Color(0xff0000,1)
